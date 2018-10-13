7926ac3013356decacaf5e1500a62f452500808a
dudummv <dudu_mmv@hotmail.com>
2018-10-13 17:31:36 -0300

    Initial invKineTest.py implementation

---
+++ ros_denso/src/invKineTest.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python2.7
+import rospy
+import numpy as np
+import sys
+import signal
+from std_msgs.msg import Float64
+
+
+
+class invKine:
+
+    def __init__(self):
+        self.thetas = np.zeros(6)
+        #self.l = [210,75,210]
+        self.l = [335, 88, 210]
+        rospy.init_node('invKineTest')
+        self.z_offset = 280
+        self.pub = []
+        self.pub.append(rospy.Publisher('/vp6242/joint1_position_controller/command', Float64, queue_size=10))
+        self.pub.append(rospy.Publisher('/vp6242/joint2_position_controller/command', Float64, queue_size=10))
+        self.pub.append(rospy.Publisher('/vp6242/joint3_position_controller/command', Float64, queue_size=10))
+        self.pub.append(rospy.Publisher('/vp6242/joint4_position_controller/command', Float64, queue_size=10))
+        self.pub.append(rospy.Publisher('/vp6242/joint5_position_controller/command', Float64, queue_size=10))
+        self.pub.append(rospy.Publisher('/vp6242/joint6_position_controller/command', Float64, queue_size=10))
+        self.publish()
+
+    def calculate_joints(self,x,y,z, display_angles = True, display_misc = True):
+        w = np.sqrt(x**2 + y**2)
+        L = np.sqrt(w**2 + z**2)
+        Lf = np.sqrt(self.l[1] ** 2 + self.l[2] ** 2)
+
+        c_phi = ((self.l[0]**2 + Lf**2) - L**2) / (2 * self.l[0] * Lf)
+        s_phi = np.sqrt(1 - c_phi**2)
+        phi = np.arctan2(s_phi, c_phi)
+
+        c_phi2 = (Lf**2 + L**2 - self.l[0]**2) / (2 * Lf * L)
+        s_phi2 = np.sqrt(1 - c_phi2**2)
+        phi2 = np.arctan2(s_phi2, c_phi2)
+
+        phi1 = np.pi - phi - phi2
+        theta_L = np.arctan2(z, w)
+
+        theta_L1 = theta_L + phi1
+        alpha = 2 * np.pi - np.arctan2(self.l[2], self.l[1]) - phi
+
+        z_kappa = np.sqrt(Lf**2 - (w - self.l[0] * np.sin(theta_L1))**2)
+        w_kappa = w - np.sin(theta_L1) * self.l[0]
+
+        kappa = np.arctan2(w_kappa, z_kappa)
+        beta = np.arctan2(self.l[2], self.l[1])
+
+        true_alpha = np.pi - beta - kappa
+
+        self.thetas[0] = np.arctan2(y, x)
+        self.thetas[1] = ((np.pi / 2) - theta_L1) * 0.8582
+        self.thetas[2] = alpha - (np.pi / 2)
+        self.thetas[3] = 0
+        self.thetas[4] = 0
+        self.thetas[5] = 0
+
+        if(display_angles):
+            print('------- GOAL -------')
+            print(' ')
+            print('x = ' + str(x) + ' y = ' + str(y) + ' z = ' + str(z))
+            print('------- JOINTS --------')
+            print()
+            print('Joint angle 1 = ' + str(self.thetas[0]))
+            print('Joint angle 2 = ' + str(self.thetas[1]))
+            print('Joint angle 3 = ' + str(self.thetas[2]))
+        if(display_misc):
+            print('-------- MISC ---------')
+            print(' ')
+            print('L = ' + str(L))
+            print('W = ' + str(w))
+            print('Lf = ' + str(Lf))
+            print('Phi = ' + str(phi*180/np.pi))
+            print('Phi1 = ' + str(phi1 * 180 / np.pi))
+            print('Phi2 = ' + str(phi2 * 180 / np.pi))
+            print('Beta = ' + str(np.arctan2(self.l[2],self.l[1])*180/np.pi))
+            print('Alpha = ' + str(alpha*180/np.pi))
+            print('W_kappa = ' + str(w_kappa))
+            print('-------- MISC ---------')
+            print(' ')
+            print(' ')
+            print(' ')
+
+    def publish(self):
+        for i in range(5):
+            theta = Float64()
+            theta.data = self.thetas[i]
+            self.pub[i].publish(theta)
+
+    def wait_msg(self):
+        while not rospy.is_shutdown():
+            try:
+                x = input('x = ')
+                y = input('y = ')
+                z = input('z = ')
+                self.calculate_joints(x,y,z)
+                self.publish()
+                rospy.sleep(2)
+
+            except KeyboardInterrupt:
+                print "Shutting down module"
+
+    def signal_handler(self,sig, frame):
+        # TODO operacoes de limpeza para quitar o programa
+        print('Quita Desgrassa')
+        sys.exit(0)
+
+    def choose(self, choice):
+        if(choice == 0):
+            self.wait_msg()
+        elif(choice == 1):
+            self.cross_move()
+        elif(choice == 2):
+            self.square_move()
+        else:
+            print('Invalide Choice!!')
+
+
+    def cross_move(self):
+        x = [250,250,250,210]
+        y = [0,125,-125,0]
+        z = [280,420,240,565]
+        z = [a - self.z_offset for a in z]
+        for i in range(3):
+            self.calculate_joints(x[i],y[i],z[i])
+            self.publish()
+            rospy.sleep(2)
+
+    def square_move(self):
+        x = [200, 250, 200, 200,210]
+        y = [200, 200, -200, -200,0]
+        z = [280, 565, 565, 280,565]
+        z = [a - self.z_offset for a in z]
+        for i in range(4):
+            self.calculate_joints(x[i], y[i], z[i])
+            self.publish()
+            rospy.sleep(2)
+
+
+if __name__ == '__main__':
+    invkine = invKine()
+    signal.signal(signal.SIGINT, invkine.signal_handler)
+    while not rospy.is_shutdown():
+        print('Choose wisely: ')
+        print('0 - For Manual Control')
+        print('1 - For Cross Move Test')
+        print('2 - For Square Move Test')
+        print(' ')
+        print(' ')
+        choice = input('Make Your Choice Youth !! >>')
+        invkine.choose(choice)
+    rospy.spin()
+
